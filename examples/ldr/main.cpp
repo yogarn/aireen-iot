f64c5d10a3ff85666a70842b66232a4710553731
Yoga Raditya <radityanalaa@gmail.com>
2025-06-18 16:24:36 +0700

    `docs:` add `ldr` code example
    Co-authored-by: ahmadnafi30 <ahmadnafim30@gmail.com>

---
+++ examples/ldr/main.cpp
@@ -0,0 +1,31 @@
+#include "config.h"
+
+#include <Arduino.h>
+#include <ldr.h>
+
+LDR ldr(LDR_PIN, LDR_THRESHOLD, LDR_REFERENCE);
+
+int ldrValue = 0;
+int ldrVoltage = 0;
+bool isLdrBright = false;
+
+void setup()
+{
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  ldrValue = ldr.readRaw();
+  ldrVoltage = ldr.readVoltage();
+  isLdrBright = ldr.isBright();
+
+  Serial.print("LDR: ");
+  Serial.print(ldrValue);
+  Serial.print(" | V: ");
+  Serial.print(ldrVoltage, 2);
+  Serial.print(" | Bright: ");
+  Serial.println(isLdrBright ? "Yes" : "No");
+
+  delay(1000);
+}

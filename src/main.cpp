1602d649889d061f42a641678d0baba909612dea
Yoga Raditya <radityanalaa@gmail.com>
2025-08-26 14:40:19 +0700

    fix: remove turbidity, ph, and temperature sensors implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,28 @@
+#include "config.h"
+
 #include <EEPROM.h>
 #include <Arduino.h>
-#include "gravity_tds.h"
-#include "ldr.h"
-#include "ph_meter.h"
-#include "temperature.h"
-#include "turbidity.h"
-#include "config.h"
+#include <gravity_tds.h>
+#include <ldr.h>
 
 GravityTDS gravityTds;
 LDR ldr(LDR_PIN, LDR_THRESHOLD, LDR_REFERENCE);
-PHMeter phSensor(PH_SENSOR_PIN);
-TemperatureSensor tempSensor(TEMP_SENSOR_PIN);
-TurbiditySensor turbSensor(TURBIDITY_PIN);
 
 float temperature = 25, tdsValue = 0;
 int ldrValue = 0;
 int ldrVoltage = 0;
 bool isLdrBright = false;
-float phValue = 0;
-float tempC = 0;
-float ntu = 0;
 
-void setup()
-{
+void setup() {
     Serial.begin(115200);
 
-    gravityTds.setPin(TDS_SENSOR_PIN);
+    gravityTds.setPin(TDS_PIN);
     gravityTds.setAref(3.3);
     gravityTds.setAdcRange(4096);
     gravityTds.begin();
-
-    phSensor.begin();
-    tempSensor.begin();
-    turbSensor.begin();
 }
 
-void loop()
-{
-    tempC = tempSensor.readTemperature();
-    temperature = tempC;
-
+void loop() {
     gravityTds.setTemperature(temperature);
     gravityTds.update();
     tdsValue = gravityTds.getTdsValue();
@@ -48,16 +31,14 @@ void loop()
     ldrVoltage = ldr.readVoltage();
     isLdrBright = ldr.isBright();
 
-    phValue = phSensor.readPH();
-    ntu = turbSensor.readNTU();
-
-    Serial.print("Temperature (C): "); Serial.println(tempC);
-    Serial.print("TDS (ppm): "); Serial.println(tdsValue, 0);
-    Serial.print("LDR: "); Serial.print(ldrValue);
-    Serial.print(" | V: "); Serial.print(ldrVoltage, 2);
-    Serial.print(" | Bright: "); Serial.println(isLdrBright ? "Yes" : "No");
-    Serial.print("pH: "); Serial.println(phValue);
-    Serial.print("Turbidity (NTU): "); Serial.println(ntu);
+    Serial.print("TDS (ppm): ");
+    Serial.println(tdsValue, 0);
+    Serial.print("LDR: ");
+    Serial.print(ldrValue);
+    Serial.print(" | V: ");
+    Serial.print(ldrVoltage, 2);
+    Serial.print(" | Bright: ");
+    Serial.println(isLdrBright ? "Yes" : "No");
     Serial.println("------------------------");
 
     delay(1000);

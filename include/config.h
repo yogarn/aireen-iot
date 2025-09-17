558b532a749b6730dc1e8b4cb6a83ce68d50e20c
Yoga Raditya <radityanalaa@gmail.com>
2025-09-17 17:01:09 +0700

    feat: add ph meter and mqtt

--- include/config.h
+++ include/config.h
@@ -1,14 +1,28 @@
+/**
+ * @brief Configuration file
+ */
+
 #pragma once
 
 // pin map
-#define LDR_PIN 2
-#define TDS_PIN 4
+#define LDR_PIN 35
+#define TDS_PIN 34
+#define PH_PIN A1
 
 // hardware constants
-#define TDS_AREF 3.3  // reference voltage on ADC, default 3.3V on ESP32
+#define TDS_AREF 3.3        // reference voltage on ADC, default 3.3V on ESP32
 #define TDS_ADC_RANGE 4096  // 4096 for 12-bit ADC
 #define LDR_REFERENCE 5.0
 
 // environmental constants
 #define TDS_TEMPERATURE 25  // default temperature for TDS calculations
 #define LDR_THRESHOLD 5
+
+// mqtt config
+#define MQTT_HOST ""
+#define MQTT_PORT 1883
+#define MQTT_CLIENT "esp1"
+
+// wifi config
+#define WIFI_SSID ""
+#define WIFI_PASS ""
